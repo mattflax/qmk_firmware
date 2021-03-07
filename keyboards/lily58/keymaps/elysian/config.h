8ab5da6924791324505880f40acf4d1a68476c9a
Matt Pearce <matt@flax.co.uk>
2021-03-07 14:10:55 +0000

    Enable function layer lock on double tap.

--- keyboards/lily58/keymaps/elysian/config.h
+++ keyboards/lily58/keymaps/elysian/config.h
@@ -33,8 +33,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
+// #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
+#define TAPPING_TOGGLE 2
 
 #undef RGBLED_NUM
 #define RGBLED_NUM 8
--- keyboards/lily58/keymaps/elysian/keymap.c
+++ keyboards/lily58/keymaps/elysian/keymap.c
@@ -11,7 +11,7 @@
 extern uint8_t is_master;
 
 enum layer_number {
-  _QWERTY = 0,
+  _QWERTY,
   _LOWER,
   _RAISE,
   _FUNCTION,
@@ -19,13 +19,12 @@ enum layer_number {
 };
 
 /* Custom keycodes for function layers */
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define FN MO(_FUNCTION)
-#define ADJUST MO(_ADJUST)
-
 enum custom_keycodes {
-  KVMSWTCH = SAFE_RANGE
+  KVMSWTCH = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  FN,
+  ADJUST
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -49,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTRL,KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
-                            KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LALT, FN \
+                            KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LALT, TT(_FUNCTION) \
 ),
 
 /* LOWER
