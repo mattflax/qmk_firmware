461ab7020ec639d460bfb1e6393bf5ede81e55d8
Matt Pearce <matt@elysiansoftware.co.uk>
2021-05-02 16:17:00 +0100

    Add caps lock, revert tap dance on lshift. Fix 5-char layer layout on OLED.

--- keyboards/sofle/keymaps/elysian/keymap.c
+++ keyboards/sofle/keymaps/elysian/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCtrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |Sft/CL|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |SftEnt|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |LCtrl | LAlt |Super |LOWER | /Space  /       \Space \  |RAISE | RCTR | RAlt | FUNC |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  TD(TD_SFT_CAPS),KC_Z,KC_X,KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
+  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
                    KC_LCTL, KC_LALT, KC_LGUI, KC_LOWER,KC_SPC,   KC_SPC, KC_RAISE,KC_RCTL, KC_RALT, TT(_FUNCTION) \
 ),
 /*
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |   {  |-------.    ,-------|   }  |   _  |   +  |   {  |   }  |Insert|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |      |   |  |      |      |      |      |-------|    |-------|   |  |   ~  |SfLeft|SfDown|ShftUp|SRight|
+ * |CapsLk|   |  |      |      |      |      |-------|    |-------|   |  |   ~  |SfLeft|SfDown|ShftUp|SRight|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      |      |LOWER | / Home  /       \ End  \  |RAISE |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, KC_DEL, \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
   _______, _______, _______, _______, _______, KC_LCBR,                    KC_RCBR, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_INS, \
-  _______, KC_NUBS, _______, _______, _______, _______, _______,  _______, KC_NUBS, KC_NUHS, S(KC_LEFT),S(KC_DOWN),S(KC_UP),S(KC_RGHT), \
+  KC_CAPS, KC_NUBS, _______, _______, _______, _______, _______,  _______, KC_NUBS, KC_NUHS, S(KC_LEFT),S(KC_DOWN),S(KC_UP),S(KC_RGHT), \
                     _______, _______, _______, _______, KC_HOME,  KC_END,  _______, _______, _______, _______\
 ),
 /* RAISE
@@ -231,13 +231,13 @@ static void print_status_narrow(void) {
             oled_write_P(PSTR("Base\n"), false);
             break;
         case _NUMPAD:
-            oled_write_P(PSTR("NumLk\n"), false);
+            oled_write_P(PSTR("NumLk"), false);  // No return at end of line
             break;
         case _RAISE:
-            oled_write_P(PSTR("Raise\n"), false);
+            oled_write_P(PSTR("Raise"), false);  // No return at end of line
             break;
         case _LOWER:
-            oled_write_P(PSTR("Lower\n"), false);
+            oled_write_P(PSTR("Lower"), false);  // No return at end of line
             break;
         case _ADJUST:
             oled_write_P(PSTR("Adj\n"), false);
