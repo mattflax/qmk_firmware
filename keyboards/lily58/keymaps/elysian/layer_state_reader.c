b8bc54c9b2ef65cff3ed64072e2205106427fae5
Matt Pearce <matt@elysiansoftware.co.uk>
2021-05-03 09:29:20 +0100

    Minor optimisation to share layer string.

--- keyboards/lily58/keymaps/elysian/layer_state_reader.c
+++ keyboards/lily58/keymaps/elysian/layer_state_reader.c
@@ -12,27 +12,29 @@
 
 char layer_state_str[24];
 
+const char* layer_label = "Layer";
+
 const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Default", layer_label);
     break;
   case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Raise", layer_label);
     break;
   case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Lower", layer_label);
     break;
   case L_FN:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Function");
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Function", layer_label);
     break;
   case L_ADJUST:
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Adjust", layer_label);
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "%s: Undef-%ld", layer_label, layer_state);
   }
 
   return layer_state_str;
