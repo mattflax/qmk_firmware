760f1698c0a1aae21e3ad6931c324abc5510e66e
Matt Pearce <matt@elysiansoftware.co.uk>
2021-12-02 18:20:07 +0000

    Layout copy for Corne

---
+++ keyboards/crkbd/keymaps/elysian/rules.mk
@@ -0,0 +1,9 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
+BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no
+
+# if firmware size over limit, try this option
+LTO_ENABLE = no
