79b267c2c44d256fd9434f557196fa267a512587
Matt Pearce <matt@elysiansoftware.co.uk>
2021-12-29 15:52:28 +0000

    Switch Alt/Fn mappings, fix workman layout.

--- keyboards/crkbd/keymaps/elysian/keymap.c
+++ keyboards/crkbd/keymaps/elysian/keymap.c
@@ -51,15 +51,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------.  ,-------------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  | ,  < | . >  | /  ? |SftEnter|
  * `----------------------+------+------+------|  |------+------+------+------+------+--------'
- *                        | Super| Lower| Space|  | Space| Raise| Alt  |
- *                        |      |      | Raise|  | Func |      |      |
+ *                        | Super| Lower| Space|  | Space| Raise| Fn   |
+ *                        |      |      | Raise|  | Alt  |      |      |
  *                        `--------------------'  `--------------------'
  */
   [_QWERTY] = LAYOUT_split_3x6_3(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_S,  KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,
-                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LT(_FUNCTION,KC_SPC),  MO(_RAISE), KC_LALT
+                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LALT_T(KC_SPC),  MO(_RAISE), MO(_FUNCTION)
 
   ),
 
@@ -103,15 +103,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_R,  KC_S,    KC_T,    KC_G,      KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,      KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,
-                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LT(_FUNCTION,KC_SPC),  MO(_RAISE), KC_LALT
-
+                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LALT_T(KC_SPC),  MO(_RAISE), MO(_FUNCTION)
   ),
 
 /*
  * Base Layer: Workman
  *
  * ,-------------------------------------------.  ,-------------------------------------------.
- * | Escape |   Q  |   D  |   R  |   W  |   B  |  |   J  |   L  |   U  |   P  |   ;  |BackSpc |
+ * | Escape |   Q  |   D  |   R  |   W  |   B  |  |   J  |   F  |   U  |   P  |   ;  |BackSpc |
  * |--------+------+------+------+------+------|  |------+------+------+------+------+--------|
  * |Ctrl/Tab|   A  |   S  |   H  |   T  |   G  |  |   Y  |   N  |   E  |   O  |   I  |  ' "   |
  * |--------+------+------+------+------+------.  ,-------------+------+------+------+--------|
@@ -122,11 +121,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `--------------------'  `--------------------'
  */
   [_WORKMAN] = LAYOUT_split_3x6_3(
-    KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,      KC_J,    KC_L,    KC_U,    KC_P, KC_SCLN, KC_BSPC,
+    KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,      KC_J,    KC_F,    KC_U,    KC_P, KC_SCLN, KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_S,  KC_H,    KC_T,    KC_G,      KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,      KC_K,    KC_L, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,
-                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LT(_FUNCTION,KC_SPC),  MO(_RAISE), KC_LALT
-
+                   KC_LGUI,  MO(_LOWER),LT(_RAISE,KC_SPC),  LALT_T(KC_SPC),  MO(_RAISE), MO(_FUNCTION)
   ),
 
 /*
