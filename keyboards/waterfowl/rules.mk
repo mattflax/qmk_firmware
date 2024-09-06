aaf20395928353215c17c7c6b4a16218257756d1
Matt Pearce <matt@elysiansoftware.co.uk>
2024-09-06 11:34:06 +0100

    Tweaks

--- keyboards/waterfowl/rules.mk
+++ keyboards/waterfowl/rules.mk
@@ -18,7 +18,7 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 TAP_DANCE_ENABLE = yes
-#DYNAMIC_TAPPING_TERM_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 
 OLED_ENABLE = yes        # Enables the use of OLED displays
