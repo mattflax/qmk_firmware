3c485f07fecc4b2b7649a18aaa333b7db981fdfd
Matt Pearce <mpearce@ebi.ac.uk>
2024-11-25 08:49:24 +0000

    Tweaks to tap term, add delete in thumb cluster.

--- keyboards/crkbd/keymaps/elysian2/keymap.c
+++ keyboards/crkbd/keymaps/elysian2/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_S,  KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SC_SENT,
-          KC_LGUI,LT(_LOWER,KC_SPC), LALT_T(KC_TAB),    KC_ENT,  LT(_RAISE,KC_BSPC), MO(_FUNCTION)
+          KC_LGUI,LT(_LOWER,KC_SPC), LALT_T(KC_TAB),    KC_ENT,  LT(_RAISE,KC_BSPC), LT(_FUNCTION,KC_DEL)
 
   ),
 
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_R,  KC_S,    KC_T,    KC_G,      KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,      KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, SC_SENT,
-          KC_LGUI,MO(_LOWER), LALT_T(KC_SPC),    KC_SPC,  MO(_RAISE), MO(_FUNCTION)
+          KC_LGUI,LT(_LOWER,KC_SPC), LALT_T(KC_TAB),    KC_ENT,  LT(_RAISE,KC_BSPC), LT(_FUNCTION,KC_DEL)
   ),
 
 /*
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,      KC_J,    KC_F,    KC_U,    KC_P, KC_SCLN, KC_BSPC,
     LCTL_T(KC_TAB),KC_A,KC_S,  KC_H,    KC_T,    KC_G,      KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,      KC_K,    KC_L, KC_COMM,  KC_DOT, KC_SLSH, SC_SENT,
-          KC_LGUI,MO(_LOWER), LALT_T(KC_SPC),    KC_SPC,  MO(_RAISE), MO(_FUNCTION)
+          KC_LGUI,LT(_LOWER,KC_SPC), LALT_T(KC_TAB),    KC_ENT,  LT(_RAISE,KC_BSPC), LT(_FUNCTION,KC_DEL)
   ),
 
 /*
