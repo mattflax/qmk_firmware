5b65601ef1e5cbdf53f38c9ec3a9face6c8820f4
mpearce <mpearce@ebi.ac.uk>
2025-01-16 14:54:35 +0000

    Remove tap-dance single quote.

--- keyboards/waterfowl/keymaps/elysian/keymap.c
+++ keyboards/waterfowl/keymaps/elysian/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|                      |------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  |                      |   H  |   J  |   K  |   L  |   ;  |
  * |------+------+------+------+------|  ,-----.    ,-----.  |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |  |CAPS |    |NUMLK|  |   N  |   M  |   ,  |   .  | /  ' |
+ * |   Z  |   X  |   C  |   V  |   B  |  |CAPS |    |NUMLK|  |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'  `-----'    `-----'  `----------------------------------'
  *          ,-----.   ,--------------------.            ,---------------------.   ,-----.
  *          |  1  |   | Esc | SPACE | TAB  |            | Enter | BS  |  Del  |   |  4  |
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
 	KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,           KC_Y,       KC_U,    KC_I,      KC_O,       KC_P,
 	LALT_T(KC_A),LGUI_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,		KC_H,RSFT_T(KC_J),LCTL_T(KC_K),LGUI_T(KC_L),LALT_T(KC_SCLN),
-	KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,           KC_N,       KC_M,   KC_COMM,    KC_DOT,     TD(TD_SLSH),
+	KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,           KC_N,       KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,
 	KC_1,  LT(_MOUSE,KC_ESC), LT(_NAV,KC_SPC), LT(_MEDIA,KC_TAB), KC_CAPS,		KC_NUM,	LT(_SYM,KC_ENT), LT(_NUM,KC_BSPC), LT(_FUNC,KC_DEL),	KC_4
 ),
 
