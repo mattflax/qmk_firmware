304f8163afe6f2c48a7863dfd5e43d10b234a198
Matt Pearce <matt@elysiansoftware.co.uk>
2026-08-29 23:19:58 +0100

    Initial changes.

---
+++ users/elysian/homerowmodkeys.h
@@ -0,0 +1,18 @@
+
+// Home row mod keys
+#define HM_A LALT_T(KC_A)
+#define HM_S LGUI_T(KC_S)
+#define HM_D LCTL_T(KC_D)
+#define HM_F LSFT_T(KC_F)
+#define HM_J RSFT_T(KC_J)
+#define HM_K LCTL_T(KC_K)
+#define HM_L LGUI_T(KC_L)
+#define HM_SCLN LALT_T(KC_SCLN)
+
+// Modifier keys
+#define L_MSESC LT(_MOUSE,KC_ESC)
+#define L_NVSPC LT(_NAV,KC_SPC)
+#define L_MEDTB LT(_MEDIA,KC_TAB)
+#define L_SYMEN LT(_SYM, KC_ENT)
+#define L_NUMBS LT(_NUM, KC_BSPC)
+#define L_FNDEL LT(_FUNC, KC_DEL)
